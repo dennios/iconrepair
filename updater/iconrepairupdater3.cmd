d3d7e17aab30e444d9b4f19d219c37e54b6407a4
Dennis <45493980+dennios@users.noreply.github.com>
2021-05-05 11:07:41 +0200

    Updater 3.0 Test Fix 2

--- updater/iconrepairupdater3.cmd
+++ updater/iconrepairupdater3.cmd
@@ -3,7 +3,10 @@
 :Udownload
 tasklist /fi "ImageName eq %iconrepairfile%" | find /i "%iconrepairfile%" >NUL
 if %errorlevel% equ 0 (if updatestat equ 0 (set updatestat=3&goto Uend) else (set updatestat=0&timeout 2 >NUL&goto Udownload))
-%S%&powershell.exe -c (invoke-webrequest -ContentType "application/octet-stream" 'https://raw.githubusercontent.com/dennios/iconrepair/master/IconRepair.exe' -outfile '%iconrepairloc%' -timeoutsec 3 -usebasicparsing)
+echo %iconrepairloc%
+echo %iconrepairdl%
+pause
+%S%&powershell.exe -c (invoke-webrequest -ContentType "application/octet-stream" '%iconrepairdl%' -outfile '%iconrepairloc%' -timeoutsec 3 -usebasicparsing)
 if %errorlevel% equ 0 (set updatestat=2) else (set updatestat=3)
 :Uend
 call "%iconrepairloc%"&exit
